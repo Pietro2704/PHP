af00e1d90a0932b900685cfcf549f98f9c8e0d5b
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-02-21 19:23:03 -0300

    versões do PHP

--- Moderno/Módulo 1/Historia/historia.php
+++ Moderno/Módulo 1/Historia/historia.php
@@ -13,18 +13,25 @@
 
     1995: -------------------------------------------------------------------------------------
     PHP 1.0 - PHP Tools
+    - Personal Home Page
+    - Funcionalidades simples para um site básico
+    - Não tinha caracteristicas de linguagem em si
 
-    1996: -------------------------------------------------------------------------------------
-    PHP/FI - Personal Home Page With Form Interpreter
+
+    1996/1997: -------------------------------------------------------------------------------------
+    PHP 2.0 - PHP/FI- Personal Home Page With Form Interpreter
+
     -- Integração de Banco
     -- Transformou em Software Livre (Open Source)
 
-    1997: -------------------------------------------------------------------------------------
-    PHP 2.0
     -- Andi Gutmans e Zeev Suraski começam a ajudar o PHP a evoluir
 
     1998: -------------------------------------------------------------------------------------
     PHP 3.0 - PHP: Hyper Processor
+
+    - Primeira versão colaborativa
+    - Características mais relacionadas a uma linguagem
+    - Extension API
 
     logo para a linguagem:
     -- Colin Viebrock fez a logo oval com php escrito por dentro
@@ -40,6 +47,64 @@
     no desenvolvimento da linguagem PHP e chamaram de 
     Zend
     
+    2000: ------------------------------------------------------------------------------------
+    PHP 4.0
+
+    - Zend Engine 1.0 (PHP começou a funcionar baseado na Zend Engine)
+    - Melhoria de preformance
+    - Aumento da modularização
+    - Uso de super globais $_GET, $_POST, $_SESSION, etc
+
+    2004: --------------------------------------------------------------------------------------
+    PHP 5.0
+
+    - Zend Engine 2.0
+    - Primeira versão com Orientação a Objeto
+    - PHP Data Objects (Conjunto de Biblioteas pra acesso a banco)
+    - Operador de exponenciação
+    - Suporte a JSON
+    - Namespaces, closures, garbage collection, exceptions handling
+    - Novo depurador (phpdbg)
+
+    2005: ---------------------------------------------------------------------------------------
+    PHP 6.0 (Uma linguagem e um sonho)
+
+    Linguagem que trouxe vários problemas agora busca dar o melhor de uma linguagem boa
+    queriam colocar: 
+
+    - Aumento da Segurança e performance
+    - Suporte Nativo a Unicode
+
+    2015: ---------------------------------------------------------------------------------------
+    PHP 7.0
+
+    - Zend Engine 3.0
+    - Performance 9x maior
+    - Escape de códigos Unicode
+    - Operador null coalescing ??
+    - Declaração escalar
+    - Operador spaceship <=> (3 comparações com o mesmo valor)
+    - Classes anônimas em orientação a Objeto
+    - Novos tipos de exeçoes
+    - Retorno de void (Retorna vazio)
+    - Libsodium (Bibloteca de criptografia de dados)
+    - Foreign function interface (Codar outras linguagens em php)
+
+    2020: -----------------------------------------------------------------------------------------
+    PHP 8.0
+
+    - Zend Engine 4.0
+    - Just-in-Time Compilation
+    - Locale-independent number convertion
+    - Named arguments
+    - Expressão match
+    - Operador nullsafe
+    - Enumerations
+    - Readonly properties
+    - Fibers
+
+
+
 
 
   */
