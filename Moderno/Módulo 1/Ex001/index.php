340937e6d1b7327d43c2b0d203b5cc8936db7f5b
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-02-22 15:08:07 -0300

    php por dentro

--- Moderno/Módulo 1/Ex001/index.php
+++ Moderno/Módulo 1/Ex001/index.php
@@ -6,13 +6,30 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dados do Servidor</title>
+  <style>
+    h1 {
+      width: fit-content;
+      margin: auto;
+      margin-block: 50px;
+    }
+  </style>
 </head>
 
+<?php
+/*
+    <?php ?> -- Super Tag PHP
+    <?  ?> -- short open tag     (Não se usa mais)
+    <%  %> -- ASP Tag           (Não se usa mais)
+    <?= ?> -- short Tag PHP
+
+  */
+?>
+
 <body>
+
   <h1>Dados do Servidor</h1>
-  <?php
-  phpinfo();
-  ?>
+
+  <?= phpinfo();  ?>
 </body>
 
 </html>
