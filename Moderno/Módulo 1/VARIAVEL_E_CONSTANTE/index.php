42b7078f3f649fd40677bcb147a2443c10577051
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-02-22 16:55:42 -0300

    vendo que php é camel sensetive

--- Moderno/Módulo 1/VARIAVEL_E_CONSTANTE/index.php
+++ Moderno/Módulo 1/VARIAVEL_E_CONSTANTE/index.php
@@ -5,11 +5,22 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Varieveis</title>
+  <title>Variaveis</title>
 </head>
 
 <body>
   <?php
+  /*
+  - Variáveis sempre começam com o símbolo $
+  - O segundo caractere (dps do cifrão) pode ser letra ou o símbolo _
+  - Aceita caracteres [a-z], [A-Z], [0-9] e [_]
+  - Aceita qualquer caracter da tabela ASCII a partir de 128
+  - Aceita caracteres acentuados como á, õ, ç...
+  - A linguagem é case sensentive
+  - Nomes especiais como $this não podem ser usados
+  */
+
+
   // PODEMOS USAR DEFINE OU CONST PARA DEFINIR UMA CONTANTE
   define("NOME", "Pietro");
   const SOBRENOME = "Rosolia";
@@ -19,7 +30,7 @@
   $regiao = "Brasil";
 
   // PRINTANDO NA TELA
-  echo "Meu nome é " . NOME . " " . SOBRENOME . " tenho $idade anos e moro no $regiao";
+  echo "<p>Meu nome é " . NOME . " " . SOBRENOME . " tenho $idade anos e moro no $regiao</p>";
 
   ?>
 </body>
