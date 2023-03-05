5e3628b61ee660ba887354d7b5d893fa8fc84747
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-03-04 22:08:29 -0300

    Lutador Feito

--- POO/aula6/Lutador.php
+++ POO/aula6/Lutador.php
@@ -21,49 +21,90 @@
 
     //construtor
 
-    public function __construct(){}
+    public function __construct($nome,$nac,$idade,$altura,$peso,$vit,$der,$emp){
+      setNome($nome);
+      setNacionalidade($nac);
+      setIdade($idade);
+      setAltura($altura);
+      setPeso($peso);
+      setVitorias($vit);
+      setDerrotas($der);
+      setEmpates($emp);
+
+    }
 
 
 
     //Get e Set
 
-    private function getNome(){}
-    private function setNome(){}
+    private function getNome(){return $this -> nome;}
+    private function setNome($a){$this -> nome = $a;}
 
-    private function getNacionalidade(){}
-    private function setNacionalidade(){}
+    private function getNacionalidade(){return $this -> nacionalidade;}
+    private function setNacionalidade($b){$this -> nacionalidade = $b;}
     
-    private function getIdade(){}
-    private function setIdade(){}
+    private function getIdade(){return $this -> idade;}
+    private function setIdade($c){$this -> idade = $c;}
 
-    private function getAltura(){}
-    private function setAltura(){}
+    private function getAltura(){return $this -> altura;}
+    private function setAltura($d){$this -> altura = $d;}
 
-    private function getPeso(){}
-    private function setPeso(){}
+    private function getPeso(){return $this -> peso;}
+    private function setPeso($e){$this -> peso = $e;}
 
-    private function getCategoria(){}
-    private function setCategoria(){}
+    private function getCategoria(){return $this -> categoria;}
+    private function setCategoria(){
+      if(getPeso()<52.2){
+        $this -> categoria = "Inválida"
+      }
+      elseif(getPeso()<=70.3){
+        $this -> categoria = "Pena"
+      }
+      elseif(getPeso()<=83.9){
+        $this -> categoria = "Médio"
+      }
+      elseif(getPeso()<=120.2){
+        $this -> categoria = "Pesado"
+      }
+      else{
+        $this -> categoria = "Inválida"
+      }
+    }
 
-    private function getVitorias(){}
-    private function setVitorias(){}
+    private function getVitorias(){return $this -> vitorias;}
+    private function setVitorias($g){$this -> vitorias = $g;}
 
-    private function getDerrotas(){}
-    private function setDerrotas(){}
+    private function getDerrotas(){return $this -> derrotas;}
+    private function setDerrotas($h){$this -> derrotas = $h;}
 
-    private function getEmpates(){}
-    private function setEmpates(){}
-
-
+    private function getEmpates(){return $this -> empates;}
+    private function setEmpates($i){$this -> empates = $i;}
 
 
-    //comportamento
 
-    public function apresentar(){}
-    public function status(){}
-    public function ganharLuta(){}
-    public function perderLuta(){}
-    public function empatarLuta(){}
+
+    // abstrato
+
+    public function apresentar(){
+      echo "Lutador: " .getNome();
+      echo "Origem: " .getNacionalidade();
+      echo getIdade() ." anos";
+      echo getAltura() ." m de altura";
+      echo "Pesando" .getPeso() ." Kg";
+      echo "Vitorias: " .getVitorias();
+      echo "Derrotas: " .getDerrotas();
+      echo "Empates: " .getEmpates();
+
+
+    }
+    public function status(){
+      echo getNome();
+      echo "É categoria do tipo " .getCategoria();
+      echo "Tem " .getVitorias() ." vitórias, " .getDerrotas() ." derrotas e " .getEmpates() ." empates";
+    }
+    public function ganharLuta(){setVitorias()+1;}
+    public function perderLuta(){setDerrotas()+1;}
+    public function empatarLuta(){setEmpates()+1;}
 
 
 
