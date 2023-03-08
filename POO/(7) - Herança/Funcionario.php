ba54926684a066fb3d819185148ab7941c8d29a0
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-03-08 19:10:16 -0300

    Classes Criadas

---
+++ POO/(7) - Herança/Funcionario.php
@@ -0,0 +1,31 @@
+<?php 
+class Funcionario extends Pessoa
+{
+  private $setor;
+  private $trabalhando;
+
+  public function getSetor(){
+    return $this->setor;
+  }
+  public function setSetor($setor){
+    $this->setor = $setor;
+    return $this;
+  }
+
+  public function getTrabalhando(){
+    return $this->trabalhando;
+  }
+  public function setTrabalhando($trabalhando){
+    $this->trabalhando = $trabalhando;
+    return $this;
+  }
+
+  public function __construct(){
+
+  }
+
+  public function mudarTrabalho(){
+    
+  }
+}
+?>
