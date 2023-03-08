4119532157ac7beeb02ee614d27f3784000fe230
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-03-08 15:52:53 -0300

    Teoria na metade

---
+++ POO/Exercícios/Teorico/Marinha_Aeronautica.php
@@ -0,0 +1,67 @@
+<?php 
+/*
+-------------------------------------------------------------------------------------------------------------------------------------------
+  (EAGS - 2009)
+
+  1) Assinale a alternativa incorreta com relação ao conceito de classe e objeto em Programação Orientada a Objeto:
+
+ -- a) Uma classe é uma instância de um objeto
+    b) Um objeto é uma construção de software que encapsula estado e comportamento
+    c) Uma classe define atributos e comportamentos compartilhados por um tipo de objeto
+    d) Em uma linguagem POO pura, tudo é um objeto, desde os tipos mais básicos
+
+
+
+  2) Assinale a alternativa incorreta sobre a definição de termos usados em POO:
+
+    a) Uma variável interna é um valor mantido dentro do objeto
+    b) Atributos são características de uma classe vísiveis externamente
+ -- c) Comportamento são as características de uma classe invisíveis externamente
+    d) O estado de um objeto é o significado combinado das variáveis internas do objeto
+
+-------------------------------------------------------------------------------------------------------------------------------------------
+  (EAGS - 2010)
+
+  1) A maioria das linguagens orientadas a objeto suporta quais níveis de acesso?
+
+    a) Secreto, Público e Primário
+ -- b) Público, Protegido e Privado
+    c) Público, Protegido e Primário
+    d) Público, Privado e Secundário
+
+-------------------------------------------------------------------------------------------------------------------------------------------
+  (EAGS - 2011)
+
+  1) No contexto dos níveis de acesso, selecione a alternativa que contém o nível que apenas garante o acesso para aquele objeto e suas subclasses:
+
+    a) Público
+ -- b) Protegido
+    c) Privado
+    d) Oculto
+
+
+
+  2) _____ é a característica da POO que permite separar o programa em várias partes menores e independentes. Cada parte possui implementação isolada e realiza seu trabalho de forma autônoma. Com essa característica é possível ocultar os detalhes internos de cada parte através de uma interface.
+
+    a) Herança
+    b) Hierarquia
+    c) Polimorfismo
+ -- d) Encapsulamento
+
+-------------------------------------------------------------------------------------------------------------------------------------------    (EAGS - 2012)
+
+  1) Correlacione as colunas e assinale a alternativa que contém a sequência 
+
+    (1) Atributo                    () Ação executada por um objeto
+    (2) Classe                      () Define os atributos e comportamentos comuns
+    (3) Comportamento               () Característica de uma classe que é visível
+    (4) Domínio                     () Contrução de um software que encapsula estado e comportamento
+    (5) Objeto                      () Espaço onde o problema reside
+
+    a) 3, 2, 4, 5, 1
+ -- b) 3, 2, 1, 5, 4
+    c) 2, 3, 1, 5, 4
+    d) 2, 4, 3, 1, 5
+
+*/
+?>
