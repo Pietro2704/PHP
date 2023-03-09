d68d232ca703b66c11f0b05c1e36f37eb94671e8
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-03-09 18:42:33 -0300

    Começando...

---
+++ POO/(8) - Polimorfismo/Parte_1/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!-- 
+    POLI = muitos
+    Morfo = forma
+
+    Polimorfismo: 
+      Permite que um mesmo nome represente vários comportamentos diferentes
+
+    Assinatura do Método: 
+      Quantidade e os tipos dos parâmetros
+
+    Tipos de Polimorfismo
+      1) Sobreposição
+      2) Sobrecarga
+      
+   -->
+</head>
+<body>
+  
+</body>
+</html>
