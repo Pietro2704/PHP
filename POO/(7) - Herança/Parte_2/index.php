3cee47de4ff2b5303e33332084e17e025d51bd90
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-03-09 13:58:08 -0300

    Começando Parte 2

---
+++ POO/(7) - Herança/Parte_2/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!-- 
+    Árvore de Herança:
+
+           -------A--------
+           |      |       |
+         --AA   --AB--    AC--
+        |       |    |        |
+        AAA   ABA   ABB     ACA
+               | 
+           ABAA ABAB
+
+      Raíz: A
+      Progenitora: São as filhas
+      Ancestral: A é ancestral de AAA
+      Descendente: ACA é descendente de A
+      Folhas: Não tem Filhos
+
+
+
+    Tipos de Herança:
+      - Herança de Implementação (mais probre)
+      - Herança para Diferença
+
+
+
+    Abstrato e Final:
+
+    Classe Abstrata:
+      Não pode ser instanciada. Só serve como progenitora
+    Método Abstrato:
+      Declarado, mas não implementado na progenitora.
+    
+    Classe Final:
+      Não pode ser herdada por outra classe. Obrigatoriamente folha
+    Método Final:
+      Não pode ser sobrescrito pelas suas sub-classes. Obrigatoriamente herdado
+
+   -->
+</head>
+<body>
+  
+</body>
+</html>
