a810c9b3d49bc1109051285bf9057444cba0abcd
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-03-09 12:53:59 -0300

    Finalizado!

--- POO/(7) - Herança/index.php
+++ POO/(7) - Herança/index.php
@@ -10,21 +10,26 @@
 
   <pre>
     <?php
+    // Requere classes
       require_once "Pessoa.php";
       require_once "Aluno.php";
       require_once "Professor.php";
       require_once "Funcionario.php";
 
+
+    // Facilita criação de objeto
       $pe = array();
       $al = array();
       $prof = array();
       $func = array();
 
+    // Criando Objetos
       $pe[] = new Pessoa();
       $al[] = new Aluno();
       $prof[] = new Professor();
       $func[] = new Funcionario();
 
+    // Colocando o principal em cada classe
       $pe[0] -> setNome("Pietro");
       $pe[0] -> setSexo("M");
       $pe[0] -> setIdade(20);
@@ -41,25 +46,22 @@
       $func[0] -> setSetor("Limpeza");
       $func[0] -> setTrabalhando(true);
 
-      $prof[0] -> ReceberAumento(50);
-      $func[0] -> demissao();
-      $al[0] -> cancelarMatricula();
+    // Alterando informações finais
+
+      //$prof[0] -> ReceberAumento(50);
+      //$func[0] -> demissao();
+      //$al[0] -> cancelarMatricula();
 
 
 
 
-
-
-
-
-
+    // Mostra oq aconteceu
       print_r($pe[0]);
       print_r($al[0]);
       print_r($prof[0]);
       print_r($func[0]);
 
 
-      
     ?>
   </pre>
 </body>
