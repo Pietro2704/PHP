ebe9ac0162271ebdc9f3e5d1f7632b43fce1cdac
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-03-17 22:10:33 -0300

    Desafio Lançado!

---
+++ POO/(9) - DesafioFinal/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <!-- 
+
+    CRIAR CLASSE VÍDEO, ATRIBUTOS PRIVADOS (título, avaliação, views, curtidas, reproduzindo), IMPLEMENTA UMA INTERFACE COM MÉTODOS PÚBLICOS (play, pause, like)
+
+    CRIAR CLASSE PESSOA, ATRIBUTOS PROTEGIDOS (nome, idade, sexo, experiencia), TEM MÉTODO PROTEGIDO (ganharEXP)
+
+    CRIAR CLASSE GAFANHOTO QUE HERDA DE PESSOA, ATRIBUTOS PRIVADOS (login e totAssistido), TEM UM MÉTODO PUBLICO (viuMaisUm)
+
+   -->
+</body>
+</html>
