bb4bcef79789e54de4aa9da9d4145977eb90f2b9
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-03-17 21:41:22 -0300

    Parte 2 - Finalizada!

--- POO/Exercícios/Teorico/Marinha_Aeronautica.php
+++ POO/Exercícios/Teorico/Marinha_Aeronautica.php
@@ -29,6 +29,28 @@
     c) Público, Protegido e Primário
     d) Público, Privado e Secundário
 
+
+
+  2) Entende-se por método abstrato um método:
+
+ -- a) Declarado, mas não implementado
+    b) Declarado e implementado
+    c) Não declarado, mas implementado
+    d) Não declarado e não implementado
+
+
+
+  3) Informe se é falso ou verdadeiro o que se afirma sobre o conceito de HERANÇA da POO
+
+    () A herança permite basear uma nova classe na definição de outra classe
+    () Não se pode sobrepor um comportamento de uma superclasse em uma subclasse
+    () Com o mecanismo de herança, é possível estabelecer relacionamentos "é um" entre classes
+
+    a) V, V, F
+--  b) V, F, V
+    c) V, V, V
+    d) F, F, V
+
 -------------------------------------------------------------------------------------------------------------------------------------------
   (EAGS - 2011)
 
@@ -48,6 +70,21 @@
     b) Hierarquia
     c) Polimorfismo
  -- d) Encapsulamento
+
+
+
+  3) Observe o diagrama a seguir e assinale a alternativa correta, no que diz respeito a classe DecimalFormat
+
+            Format
+              |
+      ___NumberFormat___
+     |                  | 
+  ChoiceFormat    DeciamlFormat   
+
+    a) Qualquer classe dada será ascendente de format
+ -- b) DecimalFormat é descendente de Format
+    c) DecimalFormat é anscestral de Format
+    d) ChoiceFormat é filha de DecimalFormat
 
 -------------------------------------------------------------------------------------------------------------------------------------------
   (EAGS - 2012)
@@ -115,6 +152,33 @@
     d) Extensível, Natural, Mensurável, Reutilizável, Instantâneo e Simples
 
 -------------------------------------------------------------------------------------------------------------------------------------------
+  (EAGS - 2016)
+
+  1) Ao programar em PHP Orientado a Objeto e declarar uma classe com a palavra-chave final, conforme o código abaixo, pode-se afirmar que:
+
+    <?php
+      final class ClasseA{
+
+      }
+    ?>
+
+    a) As subclasses de ClasseA não herdarão seus métodos
+    b) A ClasseA não poderá ser instânciada
+ -- c) A ClasseA não poderá ter subclasses
+    d) A ClasseA não poderá ter métodos
+  
+
+
+  2) Marque a alternativa que completa corretamente a lacuna.
+
+    Ao programar em PHP, utilizando-se o paradigma de orientação a objeto, utiliza-se o símbolo _____ para poder acessar as funções definidas em uma classe
+
+    a) .
+    b) >
+    c) =>
+ -- d) ->
+
+-------------------------------------------------------------------------------------------------------------------------------------------
   (CAP-PD - 2006)
 
   1) Uma casa está para uma planta arquitetônica assim como um objeto está para:
@@ -123,6 +187,16 @@
     b) Uma propriedade
  -- c) Uma classe
     d) Um atributo
+
+-------------------------------------------------------------------------------------------------------------------------------------------
+  (CAP-PD - 2007)
+
+  1) Em relação à POO, assinale a opção correta
+
+    a) Os membros private de uma superclasse são herdados pelas subclasses
+    b) Um método public da superclasse pode tornar-se um método protected ou private na subclasse
+    c) Um método protected da superclasse pode tornar-se um método private da subclasse
+ -- d) Utilizar acesso protected oferece um nível intermediario entre public e private
 
 -------------------------------------------------------------------------------------------------------------------------------------------
   (CAP-PD - 2009)
@@ -144,6 +218,17 @@
  -- d) Implementação - Interface
 
 -------------------------------------------------------------------------------------------------------------------------------------------
+  (CAP-PD - 2010)
+
+  1) Uma das formas de reutilização de um software é a criação de uma classe que absorve membros de outra classe ja existente.
+  A nova classe é aprimorada com capacidades novas ou modificadas.
+
+    a) Recursão
+ -- b) Herança
+    c) Orientação a aspesctos
+    d) Chamada por parâmetros
+
+-------------------------------------------------------------------------------------------------------------------------------------------
   (CAP-PD - 2014)
 
   1) Qual é o conceito de POO que significa representar uma entidade, incluindo apenas seus atributos mais relevantes?
@@ -163,40 +248,34 @@
  -- d) Atributos são subprogramas que definem as operações em objetos de uma classe
  
 -------------------------------------------------------------------------------------------------------------------------------------------
-  (EAGS - 2011)
 
-  1) Observe o diagrama a seguir e assinale a alternativa correta, no que diz respeito a classe DecimalFormat
-
-            Format
-              |
-      ___NumberFormat___
-     |                  | 
-  ChoiceFormat    DeciamlFormat   
-
-  a) Classes são tipos abstratos de dados
-    b) Objetos são instâncias de uma classe
-    c) Subclasse é uma classe definida por meio de outra classe
- -- d) Atributos são subprogramas que definem as operações em objetos de uma classe
-    
--------------------------------------------------------------------------------------------------------------------------------------------
   (CAP-PD - 2016)
 
-  1) Segundo Deitel (2010)com relação a Linguagem de Programção JAVA, é correto afirmar que:
+  1) Segundo Deitel (2010), com relação a Linguagem de Programção JAVA, é correto afirmar que:
 
     a) Um construtor pode ter um nome diferente de sua classe
  -- b) O compilador fornece um construtor padrão sem parâmetros em qualquer classe que não inclui explicitamente um construtor
     c) Se uma classe não incluir um construtor, as varíaveis de instância não são inicializadas
     d) não existe o conceito de construtor na linguagem JAVA
+
+
+  2) Segundo Deitel (2010), fornecer uma superclasse apropriada a partir da qual outras classes podem herdar, e assim, compartilhar um design comum, é o propósito de:
+
+    a) Um objeto
+    b) Uma subclasse
+ -- c) Uma classe abstrata
+    d) Um construtor
+
+
+
+  3) Segundo Deitel (2010), em POO, o conceito que permite escrever programas que processam objetos que compartilham a mesma superclasse
+  (dereta e indiretamente) como se todos fossem objetos dessa superclasse, é conhecido como:
+
+    a) Associação
+ -- b) Polimorfismo
+    c) Atributo
+    d) Encapsulamento
     
--------------------------------------------------------------------------------------------------------------------------------------------
-  (CAP-PD - 2010)
 
-  1) Uma das formas de reutilização de um software é a criação de uma classe que absorve membros de outra classe ja existente.
-  A nova classe é aprimorada com capacidades novas ou modificadas.
-
-    a) Recursão
- -- b) Herança
-    c) Orientação a aspesctos
-    d) Chamada por parâmetros
 */
 ?>
